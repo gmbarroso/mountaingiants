cd707c3d9795e972f94aa1a2dfd24f90c86178ca
gmbarroso <barroso.guilherme@gmail.com>
2019-05-05 02:02:49 -0300

    content: update Home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-// import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 import {
   withRouter,
 } from 'react-router-dom'
 import { compose } from 'ramda'
 
-// import Card from '../../components/Card'
+// import LeafletMap from '../Map'
 
 import './style.css'
-
-// const mapStyles = {
-//   width: '100%;',
-//   height: '100%',
-//   margin: '100px 100px',
-//   zIndex: '1',
-// };
 
 const enhance = compose(withRouter)
 
@@ -60,17 +52,21 @@ class Home extends Component {
           <div className="card pirandello" onClick={this.onAutorClick}>
             <strong>Autor</strong>
           </div>
-          <div className="card elenco" onClick={this.onGiantsClick}>
+          <div className="card elenco" onClick={this.onElencoClick}>
             <strong>Elenco</strong>
           </div>
         </div>
-        <div className="card-group" onClick={this.onElencoClick}>
-          <div className="card inspiracoes">
+        <div className="card-group">
+          <div className="card inspiracoes" onClick={this.onInspirationsClick}>
             <strong>Inspirações</strong>
           </div>
-          <div className="card gigantes" onClick={this.onInspirationsClick}>
+          <div className="card gigantes" onClick={this.onGiantsClick}>
             <strong>Os Gigantes</strong>
           </div>
+        </div>
+        <div>
+          {/* <h2>Localização</h2> */}
+          {/* <LeafletMap /> */}
         </div>
       </div>
     )
@@ -78,32 +74,3 @@ class Home extends Component {
 }
 
 export default enhance(Home)
-
-
-// = ({ google }) => {
-  // console.log(google.LatLng)
-  // const { google } = props
-//     return (
-//       <div className="home">
-
-//         <h2>Localização</h2>
-//           {/* <Map
-//             google={google}
-//             zoom={14}
-//             style={mapStyles}
-//             initialCenter={{
-//              lat: -23.5546951,
-//              lng: -46.6514999
-//             }}
-//           /> */}
-//       </div>
-
-//       // Usar rota sem hash
-//     )
-// }
-
-// export default Home
-
-// export default GoogleApiWrapper({
-//   apiKey: 'AIzaSyDITtTQheqb5O_XTfPX9HG75TtcniJegQc'
-// })(Home)
