5634d24f96a45e795e58a220fda2d11de802379e
gmbarroso <barroso.guilherme@gmail.com>
2019-05-06 17:37:37 -0300

    docs

---
+++ docs/precache-manifest.1a13714a6f63ae84dd2d672602387d5f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb7266dce3a4ac1f8e3d438b507bc66a",
+    "url": "/static/media/giantsbr1.eb7266dc.jpg"
+  },
+  {
+    "revision": "703fbbb878995f1d8799",
+    "url": "/static/css/main.5b0c73dc.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a749b49a9c14bbabb7c",
+    "url": "/static/js/2.925fcccd.chunk.js"
+  },
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/static/media/facebook_icon.3ae13bc8.svg"
+  },
+  {
+    "revision": "c15b915284e0eb6ace7fd850ec809fe2",
+    "url": "/static/media/Instagram_logo.c15b9152.svg"
+  },
+  {
+    "revision": "1e59c966ad8cf6d46715aca09d5bec8b",
+    "url": "/static/media/livro.1e59c966.jpg"
+  },
+  {
+    "revision": "50e0c3f50ecd2b9fe8ffc55ea9fa822b",
+    "url": "/static/media/livro_ptbr.50e0c3f5.jpg"
+  },
+  {
+    "revision": "52ae0b7ca60872e277baffea05ca94c3",
+    "url": "/static/media/pirandello1.52ae0b7c.jpg"
+  },
+  {
+    "revision": "9166ab98b0b02fd7fd638ef48769570b",
+    "url": "/static/media/pirandello2.9166ab98.jpg"
+  },
+  {
+    "revision": "474f296118a606ddf96b297f30523b10",
+    "url": "/static/media/pirandello3.474f2961.jpg"
+  },
+  {
+    "revision": "5830ea909ee012f917468a89ee3c871d",
+    "url": "/static/media/giantsit1.5830ea90.jpg"
+  },
+  {
+    "revision": "2ade65d2828d8b88d293a7f96b517e90",
+    "url": "/static/media/giantsit2.2ade65d2.jpg"
+  },
+  {
+    "revision": "79c5bf56db42419647040975ecdbb736",
+    "url": "/static/media/giantsit3.79c5bf56.jpg"
+  },
+  {
+    "revision": "703fbbb878995f1d8799",
+    "url": "/static/js/main.442c1411.chunk.js"
+  },
+  {
+    "revision": "ebfe3c66ffe591c05778679704f7a7c7",
+    "url": "/static/media/giantsbr2.ebfe3c66.jpg"
+  },
+  {
+    "revision": "496f106b729195682da19df768c00457",
+    "url": "/static/media/giantsbr3.496f106b.jpg"
+  },
+  {
+    "revision": "c79c63e9415d3f54237a34accc846576",
+    "url": "/static/media/giantsro1.c79c63e9.jpg"
+  },
+  {
+    "revision": "8b363e511897fc5c01ca6420a4bb05d0",
+    "url": "/static/media/giantsro2.8b363e51.jpg"
+  },
+  {
+    "revision": "bab9487e3d4432c0ccc22c42db1feed5",
+    "url": "/static/media/giantsro3.bab9487e.jpg"
+  },
+  {
+    "revision": "9ad88abd32354c1b26365a124c759be1",
+    "url": "/static/media/giant1.9ad88abd.jpg"
+  },
+  {
+    "revision": "271ec2358cc345e33c80d2d3d17527ff",
+    "url": "/static/media/giant2.271ec235.jpg"
+  },
+  {
+    "revision": "b423c505e5c6ec6af08acccebe832936",
+    "url": "/static/media/giant3.b423c505.jpg"
+  },
+  {
+    "revision": "f4e16080f405069d8b4118335cdb9b2a",
+    "url": "/static/media/giant4.f4e16080.jpg"
+  },
+  {
+    "revision": "fa856d1cd78a20273dc36460ce2908ec",
+    "url": "/static/media/giant5.fa856d1c.jpg"
+  },
+  {
+    "revision": "f0ebee6ca099f4e4d0eb8ef54bc9fe9c",
+    "url": "/static/media/giant6.f0ebee6c.jpg"
+  },
+  {
+    "revision": "90a1f34b4598c8cae8d9fff0e9589b57",
+    "url": "/static/media/elenco.90a1f34b.jpeg"
+  },
+  {
+    "revision": "e236084f6a773eb444e6f16ac3f8596e",
+    "url": "/index.html"
+  }
+];
